a440ab654f5293efec63cf1cf6ac326e97e00362
dvilli <dvilli@DESKTOP-7959SHQ>
2023-10-13 22:17:38 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using static Lab2_login_.Register;
+
+namespace Lab2_login_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин");
+            string input = Console.ReadLine();
+            Console.WriteLine("Введите пароль");
+            string input2 = Console.ReadLine();
+            Console.WriteLine("Повторите пароль");
+            string input3 = Console.ReadLine();
+            (string result, string result1) = new Register().CheckRegister(input, input2, input3);
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+        }
+    }
+}
